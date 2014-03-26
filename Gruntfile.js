232a20e0f699190430c3baaa6371168f124f4659
Moritz Lipp <mlq@pwmt.org>
2014-03-27 00:45:05 +0100

    Introduce grunt and bower for easier development

---
+++ Gruntfile.js
@@ -0,0 +1,149 @@
+module.exports = function(grunt) {
+  require('load-grunt-tasks')(grunt);
+  require('time-grunt')(grunt);
+
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+
+    bower: {
+      install: {
+      }
+    },
+
+    concat: {
+      options: {
+        separator: ';',
+      },
+      dist: {
+        src: [
+          './bower_components/jquery/jquery.js',
+          './bower_components/bootstrap/dist/js/bootstrap.js',
+          './bower_components/modernizr/modernizr.js'
+        ],
+        dest: "./dist/assets/js/<%= pkg.name %>.js"
+      }
+    },
+
+    less: {
+      dist: {
+        options: {
+          compress: true,
+        },
+        files: {
+          "./dist/assets/css/<%= pkg.name %>.css":"./<%= pkg.name %>/assets/less/style.less"
+        }
+      }
+    },
+
+    uglify: {
+      options : {
+        mangle: false,
+      },
+
+      dist: {
+        files: {
+          './dist/assets/js/<%= pkg.name %>.min.js': './dist/assets/js/<%= pkg.name %>.js'
+        }
+      }
+    },
+
+    watch: {
+      less: {
+        files: [
+          './<%= pkg.name %>/assets/less/*.less'
+        ],
+        tasks: ['less', 'cssmin'],
+      },
+      js: {
+        files: [
+          './bower_components/jquery/jquery.js',
+          './bower_components/bootstrap/dist/js/bootstrap.js',
+          './bower_components/modernizr/modernizr.js',
+          './<%= pkg.name %>/assets/js/<%= pkg.name %>.js'
+        ],
+        tasks: ['concat', 'uglify'],
+      },
+      template: {
+        files: [
+          './<%= pkg.name %>/*.lens',
+          './<%= pkg.name %>/_include/*.html',
+          './<%= pkg.name %>/_layout/*.html',
+          './<%= pkg.name %>/preview.jpg',
+          './<%= pkg.name %>/info.json'
+        ],
+        tasks: ['copy:dist'],
+        options : {
+          spawn: false,
+        }
+      }
+    },
+
+    cssmin: {
+      dist: {
+        files: {
+          './dist/assets/css/<%= pkg.name %>.min.css': [
+            './dist/assets/css/<%= pkg.name %>.css'
+          ]
+        }
+      }
+    },
+
+    copy: {
+      dist: {
+        files: [{
+          expand: true,
+          dot: true,
+          cwd: './<%= pkg.name %>',
+          dest: './dist',
+          src: [
+            '*.lens',
+            '*/*.html',
+            'preview.jpg',
+            'info.json'
+          ]
+        }]
+      },
+
+      font_awesome: {
+        files: [{
+          expand: true,
+          dot: true,
+          cwd: './<%= pkg.name %>/bower_components/font-awesome',
+          dest: './dist/assets/',
+          src: [
+            'fonts/*'
+          ]
+        }]
+      }
+    },
+
+    clean: {
+      dist: {
+        files: [{
+          dot: true,
+          src: [
+            './dist'
+          ]
+        }]
+      }
+    }
+  });
+
+  // Task definition
+  grunt.registerTask('init', [
+    'bower:install',
+  ]);
+
+  grunt.registerTask('build', [
+    'clean:dist',
+    'less',
+    'cssmin',
+    'concat',
+    'uglify',
+    'copy'
+  ]);
+
+  grunt.registerTask('default', [
+    'build'
+  ]);
+};
--- README.md
+++ README.md
@@ -9,10 +9,28 @@ Features
   * All the features of [Bootstrap](http://getbootstrap.com)
   * Built with [LESS](http://lesscss.org)
   * [Font Awesome](http://fortawesome.github.io/Font-Awesome) icons
+  * [Grunt](http://grunt.js) is used to build, preview and test your template
+  * Package management with [Bower](http://bower.io/)
   * Responsive design
 
-Templates
----------
+Installation
+------------
+
+To build koken-bootstrup just run the following commands:
+
+    $ npm install
+    $ grunt init
+    $ grunt
+
+From now on you just need to run `grunt` or `grunt build` to build the template.
+If you want to automatically detect changes and update the final output in dist,
+you can simply run `grunt watch`.
+
+Once you have build the theme, copy the "dist" directory to your
+Koken's installation `koken/storage/themes` directory to see it in action.
+
+Supported templates
+-------------------
 
   * Index - *The start page*
   * Albums - *Overview and detailed view of albums*
@@ -23,9 +41,3 @@ Templates
   * Categories - *Overview and detailed view of categories*
   * Tags - *Overview and detailed view of tags*
   * Timeline - *Timeline of every recent changes and updates*
-
-Installation
-------------
-
-Once you have downloaded the theme, copy the "koken-bootstrap" directory to your
-Koken's installation `koken/storage/themes` directory to see it in action.
